3c43a485abf5f45c39089482836a68cb9becb585
VioletaKirova <violetakirova1@gmail.com>
2019-08-02 03:37:07 +0300

    Added ReviewsController and Reviews/Create get

---
+++ Web/CookWithMe.Web.InputModels/Reviews/ReviewCreateInputModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CookWithMe.Web.InputModels.Reviews
+{
+    using CookWithMe.Services.Mapping;
+    using CookWithMe.Services.Models;
+
+    public class ReviewCreateInputModel : IMapFrom<ReviewServiceModel>
+    {
+        public string Id { get; set; }
+
+        public string Comment { get; set; }
+
+        public int Rating { get; set; }
+
+        public string RecipeId { get; set; }
+
+        public RecipeServiceModel Recipe { get; set; }
+
+        public string ReviewerId { get; set; }
+
+        public ApplicationUserServiceModel Reviewer { get; set; }
+    }
+}
---
+++ Web/CookWithMe.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,30 @@
+﻿namespace CookWithMe.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using CookWithMe.Services.Data;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class ReviewsController : BaseController
+    {
+        private readonly IRecipeService recipeService;
+
+        public ReviewsController(IRecipeService recipeService)
+        {
+            this.recipeService = recipeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(string id)
+        {
+            var recipeServiceModel = await this.recipeService.GetById(id);
+
+            this.ViewData["RecipeTitle"] = recipeServiceModel.Title;
+
+            return this.View();
+        }
+    }
+}
